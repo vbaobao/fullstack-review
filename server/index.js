7f6224e9a811a712b750b0c69565eb3d83e2eb67
vbaobao <vbao.main@gmail.com>
2021-02-10 01:35:56 -0800

    Debug POST /repos to properly handle grabbing top25 repos

--- server/index.js
+++ server/index.js
@@ -10,15 +10,29 @@ app.use(bodyParser.json());
 
 app.post('/repos', function (req, res) {
   github.getReposByUsername(req.body.user_name, (err, results) => {
-    if (err) console.error('POST /repos error: ', err);
-    db.save(results);
-    res.send('Saved repos to database.');
+    if (err) {
+      console.error('POST /repos error');
+      res.send('No such user found.');
+      return;
+    }
+    db.save(results, (err) => {
+      if (err) console.error(err.errmsg);
+      db.top25((err, results) => {
+        if (err) console.error(err.errmsg);
+        res.send('Saved repos to database.');
+      });
+    });
   })
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  db.top25((err, results) => {
+    if (err) console.err(err.errmsg);
+    console.log('SUCCESFULL TOP 25: ', results);
+  });
+
 });
 
 let port = 1128;
